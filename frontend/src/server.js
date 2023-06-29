380aeb466635e06ebd3eacfc295ae931caf2fb09
Ay-coder <ayomidebabarinde07@gmail.com>
2023-06-29 16:00:59 +0100

    deployment

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://e-shop-one-alpha.vercel.app/api/v2";
+export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "https://e-shop-one-alpha.vercel.app/";
+export const backend_url = "http://localhost:8000/";
