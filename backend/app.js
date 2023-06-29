7dcde1313cf2f20b4d20a9294b2c85244a37f213
Ay-coder <ayomidebabarinde07@gmail.com>
2023-06-29 15:50:03 +0100

    deployment

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://e-shop-v5ff.vercel.app/",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
